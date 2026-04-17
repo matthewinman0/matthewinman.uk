6700d106e660af36aa962e29710fdcd296ed77a2
Matthew Inman <matthewinman321@gmail.com>
2026-04-17 23:16:55 +0100

    fiz

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
 	plugins: [react(), cloudflare()],
+	  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        si: 'apps/si-downloader/index.html'
+      }
+    }
+  }
+})
 });
