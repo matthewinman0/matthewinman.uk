228b6a160d5c6d14440bf53dc9da70a0c10453c3
Matthew Inman <matthewinman321@gmail.com>
2026-04-17 23:27:21 +0100

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cloudflare({
-      viteEnvironment: {
-        name: "client"
-      }
-    })
-  ],
-
+  plugins: [react()],
   build: {
-    ssr: false, // 🔥 important override
-
     rollupOptions: {
       input: {
         main: "index.html",
-        mapgen: "apps/mapgen/index.html"
+        si: "apps/si-downloader/map.html"
       }
     }
   }
